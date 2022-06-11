e964da7d5a835c756ae85f86b249e22037d32976
rtokkie <natto.and.eggs@gmail.com>
2022-06-11 14:32:32 +0900

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { FireModule } from './fire/fire.module';
 import { UsersModule } from './users/users.module';
 
-const envFilePath = (process.env.NODE_ENV !== 'production' && '.env.development') || undefined;
+const envFilePath = process.env.NODE_ENV !== 'production' ? '.env.development' : undefined;
 
 @Module({
   imports: [ConfigModule.forRoot({ envFilePath }), FireModule, UsersModule],
