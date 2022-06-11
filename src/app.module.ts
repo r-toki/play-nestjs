8d5edbef52fea0b9ec7bcf253dda892164cd1203
rtokkie <natto.and.eggs@gmail.com>
2022-06-11 19:32:03 +0900

    add graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { FireModule } from './fire/fire.module';
 import { UsersModule } from './users/users.module';
@@ -7,6 +9,11 @@ import { UsersModule } from './users/users.module';
 const envFilePath = process.env.NODE_ENV !== 'production' ? '.env.development' : undefined;
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath }), FireModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver }),
+    FireModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
