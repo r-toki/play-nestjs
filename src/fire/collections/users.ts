2af1871ffb54272d858f408bd47547f6c937dc49
rtokkie <natto.and.eggs@gmail.com>
2022-06-12 09:08:24 +0900

    restrict dataloader scope to request unit

--- src/fire/collections/users.ts
+++ src/fire/collections/users.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
 import { FireCollection } from 'fire-hose-admin';
 
 import { UserData, UserDoc } from '../documents/user';
 import { FireApp } from '../fire-app';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class UsersCollection extends FireCollection<UserData, UserDoc> {
   constructor(app: FireApp) {
     super(app.db.collection('users'), (snap) => new UserDoc(snap));
