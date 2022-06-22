328c309a118d89687f2421ee6f477074f03da2bf
rtokkie <natto.and.eggs@gmail.com>
2022-06-22 09:07:57 +0900

    fix a bit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
-import { FireModule } from './fire/fire.module';
 
 const envFilePath = process.env.NODE_ENV !== 'production' ? '.env.development' : '.env';
 
-@Module({
-  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true }), FireModule, AuthModule],
-  controllers: [],
-  providers: [],
-})
+@Module({ imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true }), AuthModule] })
 export class AppModule {}
